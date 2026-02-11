bd96435026f24d8b24e24f6a8708b88f618e2294
Ahmad ashraf <ahmadashraf@Ahmads-MacBook-Pro-2.local>
2026-02-12 01:21:23 +0500

    adding swagger and making changes to block proposer

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://explorer.twilight.org',
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'wss://explorer.twilight.org',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://indexer.twilight.org',
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'wss://indexer.twilight.org',
   },
 };
 
